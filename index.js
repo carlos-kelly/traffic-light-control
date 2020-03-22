7ed6985890fed5f3e33bf87d4cd9b8c8f4e64e1c
Carlos Paelinck <me@carlos.dev>
2020-03-22 08:25:00 -0500

    Updated control flow

--- index.js
+++ index.js
@@ -5,33 +5,26 @@ const greenPin = new Gpio(21, "out");
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
-const resetPins = async () =>
-  await Promise.all([
-    redPin.write(Gpio.HIGH),
-    yellowPin.write(Gpio.HIGH),
-    greenPin.write(Gpio.HIGH),
-  ]);
-
-const performSequence = async () => {
-  await greenPin.write(Gpio.LOW);
-  await delay(7000);
-  await greenPin.write(Gpio.HIGH);
-  await yellowPin.write(Gpio.LOW);
-  await delay(2000);
-  await yellowPin.write(Gpio.HIGH);
-  await redPin.write(Gpio.LOW);
-  await delay(7000);
-  await redPin.write(Gpio.HIGH);
-};
-
-const loop = () => performSequence().then(loop);
+const reset = () => Promise.all([
+  greenPin.write(1),
+  yellowPin.write(1),
+  redPin.write(1),
+]);
 
 const main = async () => {
   try {
-    await resetPins();
-    loop();
-  } catch (err) {
-    console.error(err);
+    await reset();
+    await greenPin.write(0);
+    await delay(3000);
+    await greenPin.write(1);
+    await yellowPin.write(0);
+    await delay(2000);
+    await yellowPin.write(1);
+    await redPin.write(0);
+    await delay(3000);
+    await redPin.write(1);
+  } catch (e) {
+    console.error(e);
   }
 };
 
